9347c8cc4d065275a1ab062dcfd63dbfd3fdc2b5
Rohan R Raikar <rohanraikar7993@gmail.com>
2026-08-19 11:26:06 +0530

    feat(auth): configure SQL Server, register AuthDbContext and UserRepository, and create initial migration

--- Services/OmniCore.AuthService/Program.cs
+++ Services/OmniCore.AuthService/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using OmniCore.AuthService.Data;
+using OmniCore.AuthService.Repositories.Implementations;
+using OmniCore.AuthService.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("AuthDatabase")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
